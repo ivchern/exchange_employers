a4687451cde922f02b6f19bb367a0aafeb2260d3
ivchern <ivanchernenko2@gmail.com>
2023-07-12 21:17:43 +0300

    Fix Фикс формата даты Resource в полях fromFree endFree

--- src/main/java/com/ivchern/exchange_employers/Model/Card/Resource.java
+++ src/main/java/com/ivchern/exchange_employers/Model/Card/Resource.java
@@ -4,11 +4,14 @@ import com.ivchern.exchange_employers.Model.BaseEntity;
 import com.ivchern.exchange_employers.Model.Team.Skill;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 import java.util.Set;
@@ -23,8 +26,12 @@ public class Resource extends BaseEntity {
     private String description;
     @Column(name = "location_worked")
     private String locationWorked;
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    @Temporal(TemporalType.DATE)
     @Column(name = "from_free")
     private Date fromFree;
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    @Temporal(TemporalType.DATE)
     @Column(name = "end_free")
     private Date endFree;
     @Column(name = "teammate_id")
