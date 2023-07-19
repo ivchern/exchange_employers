7fa7d3f0ee9ff495ab8c86da503292e2f0a551f7
ivchern <ivanchernenko2@gmail.com>
2023-07-19 16:25:21 +0300

    Fix

--- src/main/java/com/ivchern/exchange_employers/Repositories/TeamRepository.java
+++ src/main/java/com/ivchern/exchange_employers/Repositories/TeamRepository.java
@@ -5,10 +5,12 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+@Repository
 public interface TeamRepository extends CrudRepository<Team, Long> {
     Optional<Team> findByName(String name);
     Optional<Team> findByOwnerId(Long id);
--- src/main/java/com/ivchern/exchange_employers/Repositories/UserDetailsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ivchern.exchange_employers.Repositories;
-
-import com.ivchern.exchange_employers.Model.User.OwnerDetail;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserDetailsRepository extends CrudRepository<OwnerDetail, Long> {
-}
