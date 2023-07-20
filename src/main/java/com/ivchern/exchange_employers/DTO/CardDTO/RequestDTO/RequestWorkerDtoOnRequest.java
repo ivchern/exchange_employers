aee138b3bf7c925ff50ef8543c49f1159f86b451
ivchern <ivanchernenko2@gmail.com>
2023-07-20 22:05:50 +0300

    fix RequestWorker

--- src/main/java/com/ivchern/exchange_employers/DTO/CardDTO/RequestDTO/RequestWorkerDtoOnRequest.java
+++ src/main/java/com/ivchern/exchange_employers/DTO/CardDTO/RequestDTO/RequestWorkerDtoOnRequest.java
@@ -1,5 +1,6 @@
 package com.ivchern.exchange_employers.DTO.CardDTO.RequestDTO;
 
+import com.ivchern.exchange_employers.Model.Status;
 import com.ivchern.exchange_employers.Model.Team.Skill;
 import com.ivchern.exchange_employers.Model.User.OwnerDetails;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -7,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Set;
 
@@ -25,5 +27,8 @@ public class RequestWorkerDtoOnRequest{
     boolean isInterviewNeeded;
     private OwnerDetails ownerDetail;
     private Set<Skill> skills;
+    private LocalDateTime created;
+    private LocalDateTime updated;
+    private Status status;
 }
 
