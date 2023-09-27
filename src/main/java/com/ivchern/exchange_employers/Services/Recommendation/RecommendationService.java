6b087b076adc324242d64f74cda2db2e691dd689
ivchern <ivanchernenko2@gmail.com>
2023-09-27 15:45:38 +0300

    update docker

--- src/main/java/com/ivchern/exchange_employers/Services/Recommendation/RecommendationService.java
+++ src/main/java/com/ivchern/exchange_employers/Services/Recommendation/RecommendationService.java
@@ -1,18 +1,20 @@
 package com.ivchern.exchange_employers.Services.Recommendation;
 
 import com.ivchern.exchange_employers.Configuration.RecommendationSystemClient;
+import com.ivchern.exchange_employers.Configuration.VariableEnvironment;
 import com.ivchern.grpc.Recommendations.*;
 import org.springframework.stereotype.Service;
 
-import javax.net.ssl.SSLException;
 import java.io.IOException;
 
 @Service
 public class RecommendationService {
     private final RecommendationSystemClient recommendationSystemClient;
+    private final VariableEnvironment variableEnvironment;
 
-    public RecommendationService() throws IOException {
-        recommendationSystemClient = new RecommendationSystemClient("127.0.0.1", 50051);
+    public RecommendationService(VariableEnvironment variableEnvironment) throws IOException {
+        this.variableEnvironment = variableEnvironment;
+        recommendationSystemClient = new RecommendationSystemClient(variableEnvironment.getGrpcAddress(), variableEnvironment.getGrpcPort());
     }
 
     public CardResponse getRecommendation(CardRequest request) {
